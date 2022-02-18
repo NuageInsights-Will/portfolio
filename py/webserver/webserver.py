548a82e18a4523fbfda0d22bbf2a855f5a494d45
NuageInsights-Will <97305735+NuageInsights-Will@users.noreply.github.com>
2022-02-18 13:32:11 -0500

    cleaned up webserver.py
    according to feedback given: converted list to dict where appropriate, added type hint and docstring, and linted code using black.

--- py/webserver/webserver.py
+++ py/webserver/webserver.py
@@ -3,47 +3,45 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-icecreams = [{
-    "name" : "MrSmith",
-    "flavour" : "Vanilla",
-    "price" : 3.50
-    }, 
-    {
-    "name" : "MrsSmith",
-    "flavour" : "Chocolate",
-    "price" : 3.50
-    }, 
-    {
-    "name" : "Matcha",
-    "flavour" : "Green Tea",
-    "price" : 5.50
-    }]
+icecreams = {
+    "MrSmith": {"flavour": "Vanilla", "price": 3.50},
+    "MrsSmith": {"flavour": "Chocolate", "price": 3.50},
+    "Matcha": {"flavour": "Green Tea", "price": 5.50},
+}
 
-users = [{
-    "name" : "Vivian", 
-    "email" : "vivian.d@gmail.com"
-    }, {
-    "name" : "Erica", 
-    "email" : "erica.d@gmail.com"
-    }]
+users = [
+    {"name": "Vivian", "email": "vivian.d@gmail.com"},
+    {"name": "Erica", "email": "erica.d@gmail.com"},
+]
+
 
 @app.route("/", methods=["GET"])
 def index():
-    return jsonify({"message" : "Hello, world!"})
+    return jsonify({"message": "Hello, world!"})
+
 
 @app.route("/flavours", methods=["GET"])
 def show_all_flavours():
-    #returns all the values in the icecream_flavours list
-    return jsonify({"icecream_flavours" : icecreams})
+    # returns all the values in the icecream_flavours list
+    return jsonify({"icecream_flavours": icecreams})
+
 
 @app.route("/flavours/<string:name>", methods=["GET"])
-def show_one_flavour(name):
-    #returns the flavour with the matching name
-    flavours = [flavours for flavours in icecreams if flavours["name"] == name]
-    return jsonify({"icecream" : flavours[0]})
+def show_one_flavour(name: str):
+    """_summary_
+
+    Args:
+        name (str): name of the flavour
+
+    Returns:
+        _type_: json dict with the specified flavour
+    """
+    flavours = icecreams[name]
+    return jsonify({"icecream": flavours})
+
 
 @app.route("/signup", methods=["POST"])
 def signup():
-    new_user = {"name" : request.json["name"], "email" : request.json["email"]}
+    new_user = {"name": request.json["name"], "email": request.json["email"]}
     users.append(new_user)
-    return jsonify({"users" : users})
+    return jsonify({"users": users})
